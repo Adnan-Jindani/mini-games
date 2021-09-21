507b783f02c138f2e13d13ba778b2a1efb39f5a1
Adnan-Jindani <88643855+Adnan-Jindani@users.noreply.github.com>
2021-09-21 10:53:25 +0530

    Add files via upload

---
+++ spy 2.py
@@ -0,0 +1,21 @@
+#imports
+from guizero import App, Text, PushButton
+from random import choice
+
+#Function
+def choose_name():
+    #print("The button was pressed")
+    first_names = ["Virat", "Samrat", "Pakhi", "Saee", "Bhawani", "Mohit"]
+    last_names = ["Joshi", "Deshpande", "Chavan", "Singh"]
+    spy_name = choice(first_names) + " " + choice(last_names)
+    print(spy_name)
+
+#App
+app = App("TOP SECRET")
+
+#WIDGETS
+title = Text(app, "Push the red button to find out your spy name")
+button = PushButton(app, choose_name, text = "Tell me!")
+
+#display
+app.display()
